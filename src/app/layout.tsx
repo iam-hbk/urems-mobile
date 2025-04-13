1419362fbe579c65edde43394a79d73c457e8888
iam-hbk <rainheritier@gmail.com>
2025-04-13 13:15:34 +0200

    proper layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,10 +5,23 @@ import "./print.css";
 import { cn } from "@/lib/utils";
 import { Toaster } from "@/components/ui/sonner";
 import QueryClientWrapper from "@/lib/react-query-client";
-import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/app-sidebar";
-import { ScrollArea } from "@/components/ui/scroll-area";
-import { Scroll } from "lucide-react";
+import { Menu, Scroll } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -37,7 +50,33 @@ export default function RootLayout({
         <QueryClientWrapper>
           <SidebarProvider defaultOpen={false}>
             <AppSidebar />
-            <SidebarInset>{children}</SidebarInset>
+            {/* <SidebarTrigger className="m-4 md:hidden fixed top-0 left-0">
+              <Button variant="ghost" size="icon">
+                <Menu className="h-6 w-6" />
+              </Button>
+            </SidebarTrigger> */}
+            <SidebarInset>
+              <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+                <SidebarTrigger className="-ml-1 w-4 h-4 text-gray-500" />
+                <Separator orientation="vertical" className="mr-2 h-4" />
+                <Breadcrumb>
+                  <BreadcrumbList>
+                    <BreadcrumbItem className="hidden md:block">
+                      <BreadcrumbLink href="#">
+                        Dashboard
+                      </BreadcrumbLink>
+                    </BreadcrumbItem>
+                    {/* <BreadcrumbSeparator className="hidden md:block" />
+                    <BreadcrumbItem>
+                      <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                    </BreadcrumbItem> */}
+                  </BreadcrumbList>
+                </Breadcrumb>
+              </header>
+              <div className="flex flex-1 flex-col">
+                {children}
+              </div>
+            </SidebarInset>
           </SidebarProvider>
           <Toaster />
         </QueryClientWrapper>
